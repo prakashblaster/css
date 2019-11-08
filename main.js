b0c0288c50622cf601cef9b9e2a1e54e74790bfc
prakash <prakashbscit6396@gmail.com>
2019-11-08 18:25:33 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// const list = document.querySelector('ul');
+// const newLine = document.createElement('li');
+// newLine.textContent = 'five';
+
+// list.appendChild(newLine);
+
+
+// function contactUs(yourId) {
+//     console.log(yourId.id);
+//     document.getElementById(yourId.id).remove();
+//     let container1 = document.createElement('div');
+//     container1.setAttribute('id', 'inner-container-1');
+// }
